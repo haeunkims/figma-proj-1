d59161fa67b0779bb1c67fdaf47cc190f9d110a8
Haeun Kim <gracekim.an@gmail.com>
2022-01-25 17:33:10 +0900

    nav and hero starT

---
+++ src/Hero.js
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+function Hero() {
+  return (
+    <div>
+      <Nav />
+      <h3>Introduce Your Product Quickly and Effectively </h3>
+      <p>
+        It seemed like it should have been so simple. There was nothing
+        inherently difficult with getting the project done. It was simple and
+        straightforward enough that even a child should have been able to
+        complete it on time, but that wasn't the case. The deadline had arrived
+        and the project remained unfinished.
+      </p>
+    </div>
+  );
+}
+export default Hero;
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+  return (
+    <nav>
+      <p>Home</p>
+      <p>About </p>
+      <p>Contact</p>
+      <p>Landie</p>
+      <p>Buy now</p>
+    </nav>
+  );
+}
+export default Nav;
