560cbca2885c4d801678ce88eb3ead2a8009263b
Haeun Kim <gracekim.an@gmail.com>
2022-02-03 18:30:11 +0900

    CSS for Hero and Nav

--- src/Hero.js
+++ src/Hero.js
@@ -3,27 +3,35 @@ import "./Hero.css";
 import group from "./IMGS/Group.png";
 function Hero() {
   return (
-    <div className="Hero-page">
+    <div className="Hero">
       <Nav />
-      <div className="Hero-content">
-        <div className="Hero-context">
-          <h3>Introduce Your Product Quickly and Effectively </h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-            Nulla consequat massa quis enim.
-          </p>
+      <header className="row">
+        <div className="column">
+          <div className="Hero-context">
+            <div className="Hero-para">
+              <h3>Introduce Your Product Quickly and Effectively </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+                quis, sem. Nulla consequat massa quis enim.
+              </p>
+            </div>
+            <div className="context-btn">
+              <a className="btn btn-full" href="#">
+                Purchase UI Kit
+              </a>
+              <a className="btn btn-ghost" href="#">
+                Learn More
+              </a>
+            </div>
+          </div>
         </div>
-        <button className="purchase-btn">Purchase UI Kit</button>
-        <button className="learn-btn">Learn More</button>
-      </div>
-      <div className="path">
-        <div className="group">
-          <img src={group} />
+        <div className="column">
+          <img className="group" src={group} />
         </div>
-      </div>
+      </header>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -2,19 +2,23 @@ import "./Nav.css";
 function Nav() {
   return (
     <nav className="nav">
-      <div className="links">
-        <ul class="nav-links">
-          <li className="nav-item">Home</li>
-          <li className="nav-item">About</li>
-          <li className="nav-item">Contacts</li>
-          <li className="nav-item">
-            <h6>Landie</h6>
-          </li>
-          <li className="nav-item">
-            <button className="buy-btn">Buy Now!</button>
-          </li>
-        </ul>
-      </div>
+      <ul class="main-nav">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contacts</a>
+        </li>
+      </ul>
+      {/* <ul className="landie">
+        <li>Landie</li>
+      </ul> */}
+      <a className="btn-buy" href="#">
+        Buy Now!
+      </a>
     </nav>
   );
 }
